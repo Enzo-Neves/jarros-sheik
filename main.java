74399f5e08f749b4f4d9932bd5d1be157a2bd4f4
Enzo-Neves <enzo.neves93@gmail.com>
2022-08-17 18:34:13 -0300

    Casos de teste 1x e 2x feitoss

--- main.java
+++ main.java
@@ -10,39 +10,83 @@ public static void main(String[] args) {
     int capacidadejarro1 , capacidadejarro2 , capacidadejarro3;
     int aguajarro1 , aguajarro2 , aguajarro3;
     int quantidadeaguadesejada1,quantidadeaguadesejada2,quantidadeaguadesejada3; 
-    
+    int cont = 0;
     String casoteste = "casoteste.txt";
+    String casotesteB = "casotesteB.txt";
+    String casoteste1M= "Casos1m.txt";
+    String casogrande= "CasosMtgrande.txt";//casos mt grandes também não estão a funcionar corretamente;
+    //aprincipio eles estouram os limites de inteiros. 
+    String casotesteC = "casotesteC.txt"; //caso C não esta funcionando 
+    //aprincipio não funciona quando precisa voltar de um JarroB para um jarroA 
+    //motivo não tem função para.
 
-    try (BufferedReader br= new BufferedReader(new FileReader(casoteste))) {
-        String line = br.readLine();
+    // TODO resolver casos que o processamento é diferente dos casos de teste e colocar para todos os casos
+
+    try (BufferedReader br= new BufferedReader(new FileReader(casogrande))) {
+            String line = br.readLine();
             String[] linha = line.split(" ");
             capacidadejarro1 = Integer.parseInt(linha[0]);
             capacidadejarro2 = Integer.parseInt(linha[1]);
             capacidadejarro3 = Integer.parseInt(linha[2]);
-            System.out.println("linha1: "+line);
             line = br.readLine();
-            System.out.println("linha2: "+line);
             linha = line.split(" ");
             aguajarro1 = Integer.parseInt(linha[0]);
             aguajarro2 = Integer.parseInt(linha[1]);
             aguajarro3 = Integer.parseInt(linha[2]);
             line = br.readLine();
-            System.out.println("linha3: "+line);
             linha = line.split(" ");
             quantidadeaguadesejada1 = Integer.parseInt(linha[0]);
             quantidadeaguadesejada2 = Integer.parseInt(linha[1]);
             quantidadeaguadesejada3 = Integer.parseInt(linha[2]);
-
+            //JA
+            int[] jarro1 = new int[3];
+            int[] jarro2 = new int[3];
+            int[] jarro3 = new int[3];
+            jarro1[0] = capacidadejarro1;
+            jarro1[1] = aguajarro1;
+            jarro1[2] = quantidadeaguadesejada1;
+            //JB
+            jarro2[0] = capacidadejarro2;
+            jarro2[1] = aguajarro2;
+            jarro2[2] = quantidadeaguadesejada2;
+            //JC
+            jarro3[0] = capacidadejarro3;
+            jarro3[1] = aguajarro3;
+            jarro3[2] = quantidadeaguadesejada3;
             //TODO:
             //se a quantidade que esta no vaso for menor que a desejada preencher com o que falta
             //se a quantidade colocada for maior do que a capacidade total retirar agua daquele jarro
             //se a soma de duas aguas forem menores que a capacidade do jarro e completar o que falta para 
             //a quantidade desejada, despejar agua do vazoA em vasoB 
             //so posso esvaziar o vazo ate a borda sem desperdiçar agua
-            if(aguajarro1 < quantidadeaguadesejada2 && aguajarro1+ aguajarro2 < capacidadejarro2){
-                aguajarro2 += aguajarro1; 
+            //ex: 
+            // 6 10 15
+            // 5 7 8
+            // 0 5 15
+            //==============  JB -> JC,  JA -> JB
+            // 6 10 15
+            // 0 5  15
+            // 0 5  15
+            while(true){
+            if(jarro1[1] == jarro1[2])
+                    break;
+        
+            if(jarro1[1] <= jarro2[2] && jarro2[1]+ jarro1[1] <= jarro2[0]){
+                jarro2[1] += jarro1[1];
+                jarro1[1] -= jarro1[1]; 
+                cont += 1;
             }
+            else if(jarro2[1] <= jarro3[2] && jarro3[1]+ jarro2[1] <= jarro3[0]){ 
+                jarro3[1] += jarro2[1];
+                jarro2[1] -= jarro2[1]; 
+                cont += 1;
 
+            }
+        }
+        System.out.println("Jarro1: \n" + "Quantidade total: " + jarro1[0] + "\n quanto tem no jarro: "+ jarro1[1]+ "\nQuantidade desejada: "+ jarro1[2]);
+        System.out.println("\nJarro2: \n" + "Quantidade total: " + jarro2[0] + "\n quanto tem no jarro: "+ jarro2[1]+ "\nQuantidade desejada: "+ jarro2[2]);
+        System.out.println("\nJarro3: \n" + "Quantidade total: " + jarro3[0] + "\n quanto tem no jarro: "+ jarro3[1]+ "\nQuantidade desejada: "+ jarro3[2]);
+        System.out.print("=======\n A quantidade de movimentos realizada foi: "+cont);
     }
     catch(IOException e){
         System.out.println("Error:" + e.getMessage());
